86df34c76571d07e338b76e8bf9f2db0bbf91c94
caohanyang <caohanyang007@gmail.com>
2015-06-09 10:35:50 +0200

    modify the fname

--- crawler.js
+++ crawler.js
@@ -1,4 +1,4 @@
-var url="http://www.amazon.fr";
+var url="http://www.amazon.fr/";
 
 var fs=require('fs');
 var page = require('webpage').create();
@@ -10,17 +10,15 @@ page.captureContent = [ /.*/ ];
 page.onResourceReceived = function(response) {
 
 	
-	// console.log('Response (#' + response.id + ', stage "' + response.stage + '"): ' + JSON.stringify(response));
+    //console.log('Response (#' + response.id + ', stage "' + response.stage + '"): ' + JSON.stringify(response));
 	if (response.stage!="end" || !response.bodySize) return;
-
-	if (response.contentType.indexOf("javascript") != -1 || response.contentType.indexOf("css") != -1 || response.contentType.indexOf("html") != -1){
+	if (response.contentType.indexOf("javascript") != -1 || response.contentType.indexOf("css") != -1 || response.contentType.indexOf("json") != -1 || response.contentType.indexOf("html") != -1){
 		// var matches = response.url.match(/[/]([^/]+)$/);
-		var matches = response.url.match(/[^/]+$/);
+		var matches = response.url.match(/[^/]+[/]*$/);
 		// var fname = "contents/"+matches[1];
-		var fname = "contents/"+matches[0];
+		var fname = "contents/"+matches[0].split("/")[0];
+	    console.log('Response (#' + response.id + ', stage "' + response.stage + '"): ' + JSON.stringify(response));
 
-		console.log(response.contentType);
-		// console.log("Saving "+response.bodySize+" bytes to "+fname);
 		fs.write(fname, response.body);		
 	}
 };
