57dcb677e6788e3ae4d1262122e1fc0d4c9c12c5
caohanyang <caohanyang007@gmail.com>
2015-06-09 11:46:00 +0200

    add script

---
+++ myScript.sh
@@ -0,0 +1,22 @@
+#! /bin/bash          
+array=()
+
+# Read the file in parameter and fill the array named "array"
+getArray() {
+    i=0
+    while read line # Read a line
+    do
+    	echo $line
+        array[i]=$line # Put it into the array
+        i=$(($i + 1))
+    done < $1
+}
+
+getArray "top100.txt"
+
+for e in "${array[@]}"
+do
+    slimerjs crawler.js "$e"
+done
+
+
