2d0c4359fa5e167f578fd6d11daeb76c86a219ae
caohanyang <caohanyang007@gmail.com>
2015-06-18 15:19:19 +0200

    use 2 methods to detect version

--- myScript.sh
+++ myScript.sh
@@ -1,7 +1,13 @@
-#! /bin/bash          
+#! /bin/bash   
+
+export SLIMERJSLAUNCHER=/usr/bin/firefox
+
+PATH=/usr/local/heroku/bin:/home/caohanyang/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+export PATH
+
 array=()
 
-
+cd /home/caohanyang/2015Second/WebCrawler/
 # Read the file in parameter and fill the array named "array"
 getArray() {
     i=0
@@ -18,14 +24,18 @@ getArray "url.txt"
 for e in "${array[@]}"
 do
     slimerjs --ssl-protocol=any crawler.js "$e"
+    phantomjs --ssl-protocol=any detectVersion.js "$e"
 done
 
+
+myDate=$(date +%Y%m%d)
+
 #move all the directories 
-mkdir $1
+mkdir $myDate
 
 #var directories=echo ls -d *
 i=`ls -d */`
 
 #echo $i
 
-mv $i $1
+mv $i $myDate
