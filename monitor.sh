da21b92f3f25f2f3fcfc62d8667043fa3714acab
kenmick <louisluchen@gmail.com>
2015-07-06 18:31:48 +0200

    improve automation

---
+++ monitor.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+for ((i=1;i<=200;i++))
+do
+	sleep 10
+	ps -ef | grep "firefox" | grep -v "grep"
+	pid=`ps -ef | grep "firefox" | grep -v "grep" | awk '{print $2}'`
+	run_time=`ps -eo pid,etime | grep $pid | awk '{print $2}'`
+	run_min=`echo $run_time | cut -d ':' -f 1`
+	echo $run_time
+	if [ "$run_min"x = "08"x ]; then
+		kill -9 $pid
+	fi
+done
+
+
+
